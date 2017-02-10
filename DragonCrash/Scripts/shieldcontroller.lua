86a206ff45b17323b3cc6eac817b9df9639348e0
Scott Larsen McLean <slmclean@crm.ucsc.edu>
2017-02-09 19:25:06 -0800

    Updated shield script and inputbinding.

--- DragonCrash/Scripts/shieldcontroller.lua
+++ DragonCrash/Scripts/shieldcontroller.lua
@@ -29,6 +29,7 @@ function shieldcontroller:OnActivate()
 	activateScaling = Transform.CreateScale(Vector3(80,80,40));
 	deactivateScaling = Transform.CreateScale(Vector3(0.01,0.01,0.01));
 	self.transformSender:SetLocalTM(activateScaling);
+	timer = 0.0;
 
 end
 
@@ -41,11 +42,19 @@ end
 function shieldcontroller:OnTick(deltaTime)
 	-- TODO: add shielding logic
 	
+	if(timer > 1000) then
+		self.StateValues.Shield = not self.StateValues.Shield;
+		timer = 0;
+	end
+	timer = timer + 1;
+	
 	if(self.StateValues.Shield) then
-		
+		Debug.Log("yes shield");
+		self.transformSender:SetLocalTM(activateScaling);
 	end
 	
-	if (not self.InputValues.IShield) then
+	if (not self.StateValues.Shield) then
+		Debug.Log("no shield");
 		self.transformSender:SetLocalTM(deactivateScaling);
 	end
 
